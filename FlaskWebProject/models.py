1b65ce4da470bf8e852026195a266464ec3f2505
Son Pham <thaison.pham092@gmail.com>
2023-09-08 13:12:46 +0700

    update version

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -7,9 +7,12 @@ import string, random
 from werkzeug.utils import secure_filename
 from flask import flash
 
+# blob_container = app.config['BLOB_CONTAINER']
+# blob_service = BlobServiceClient.from_connection_string(app.config['BLOB_CONNECTION_STRING'])
+# container_client = blob_service.get_container_client(blob_container)
 blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlobServiceClient.from_connection_string(app.config['BLOB_CONNECTION_STRING'])
-container_client = blob_service.get_container_client(blob_container)
+storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
+blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['BLOB_STORAGE_KEY'])
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
